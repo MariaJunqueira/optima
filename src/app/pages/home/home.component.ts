11bd43a6d9cd66a093ef3a9e96e0103aaefcc5b1
Maria Junqueira <mariaa.junqueira@gmail.com>
2024-06-15 00:07:06 +0200

    adding article component and adjust main height

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
+import { ArticleComponent } from './article/article.component';
 import { ListComponent } from './list/list.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ListComponent],
+  imports: [ArticleComponent, ListComponent, TranslateModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
