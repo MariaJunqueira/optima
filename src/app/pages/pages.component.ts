e26f03d0aae17f3395d3fd35350792d823ae01b5
Maria Junqueira <mariaa.junqueira@gmail.com>
2024-06-14 22:01:36 +0200

    adding highlight style

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -4,18 +4,20 @@ import { RouterModule } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 
 import { GridDebugComponent } from '../shared/components/grid-debug/grid-debug.component';
+import { HeaderComponent } from '../shared/components/header/header.component';
+import { HighlightComponent } from '../shared/components/highlight/highlight.component';
 import { CookieService } from '../shared/services/cookie/cookie.service';
 import { COOKIE_LANG } from '../shared/services/language/language.constant';
 
 @Component({
   selector: 'app-pages',
   standalone: true,
-  imports: [RouterModule, CommonModule, GridDebugComponent],
+  imports: [RouterModule, CommonModule, GridDebugComponent, HeaderComponent, HighlightComponent],
   templateUrl: './pages.component.html',
   styleUrls: ['./pages.component.scss'],
 })
 export class PagesComponent {
-  constructor(public translate: TranslateService, private cookieService: CookieService) {}
+  constructor(public translate: TranslateService, private cookieService: CookieService) { }
 
   onLangChange(lang: string) {
     this.translate.use(lang);
