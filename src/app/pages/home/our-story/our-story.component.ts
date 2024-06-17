81ce183bb20857925a5aa7a5f74c738e772d25ce
Maria Junqueira <mariaa.junqueira@gmail.com>
2024-06-17 20:14:10 +0200

    adding our story component

---
+++ src/app/pages/home/our-story/our-story.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-our-story',
+  standalone: true,
+  imports: [TranslateModule],
+  templateUrl: './our-story.component.html',
+  styleUrl: './our-story.component.scss'
+})
+export class OurStoryComponent {
+
+}
