783941e59ae3de5bdb2df84d8ab2deedcac12ba9
Maria Junqueira <mariaa.junqueira@gmail.com>
2024-06-30 21:53:31 +0200

    replacing button with app button

--- src/app/pages/home/list/list.component.ts
+++ src/app/pages/home/list/list.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
+import { ButtonComponent } from '../../../shared/components/button/button.component';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [ButtonComponent, TranslateModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
-export class ListComponent {
-
-}
+export class ListComponent { }
