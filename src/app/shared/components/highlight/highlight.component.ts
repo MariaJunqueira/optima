9bdd75f3225c704e8e51c36dc1f3f8af7437a7dd
Maria Junqueira <mariaa.junqueira@gmail.com>
2024-06-30 21:52:01 +0200

    replacing button with app button

--- src/app/shared/components/highlight/highlight.component.ts
+++ src/app/shared/components/highlight/highlight.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
+import { ButtonComponent } from '../button/button.component';
 
 @Component({
   selector: 'app-highlight',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [ButtonComponent, TranslateModule],
   templateUrl: './highlight.component.html',
   styleUrl: './highlight.component.scss'
 })
