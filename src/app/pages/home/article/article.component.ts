7d20738bc879fef597cbb83ee71e8a262df69057
Maria Junqueira <mariaa.junqueira@gmail.com>
2024-06-30 23:22:23 +0200

    spacing adjustment

--- src/app/pages/home/article/article.component.ts
+++ src/app/pages/home/article/article.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
+import { ButtonComponent } from '../../../shared/components/button/button.component';
 
 @Component({
   selector: 'app-article',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [ButtonComponent, TranslateModule],
   templateUrl: './article.component.html',
   styleUrl: './article.component.scss'
 })
