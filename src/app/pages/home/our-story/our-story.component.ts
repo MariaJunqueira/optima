8f00eb803c3542ff251cc901c34a1d668b12ba46
Maria Junqueira <mariaa.junqueira@gmail.com>
2024-07-20 20:37:40 +0200

    adjust spacings and grid for blocks

--- src/app/pages/home/our-story/our-story.component.ts
+++ src/app/pages/home/our-story/our-story.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
+import { TestimonialComponent } from './testimony/testimonial.component';
 
 @Component({
   selector: 'app-our-story',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [TestimonialComponent, TranslateModule],
   templateUrl: './our-story.component.html',
   styleUrl: './our-story.component.scss'
 })
